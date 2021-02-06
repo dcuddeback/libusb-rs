99df435813a2c0740345077d481dfebf586e3eac
Richo Healey <richo@psych0tik.net>
2021-02-05 18:57:11 -0800

    Add an example

---
+++ examples/hotplug_events.rs
@@ -0,0 +1,27 @@
+extern crate libusb;
+
+use std::error::Error;
+use std::slice;
+use std::str::FromStr;
+use std::time::Duration;
+
+#[derive(Debug)]
+struct Endpoint {
+    config: u8,
+    iface: u8,
+    setting: u8,
+    address: u8
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut ctx = libusb::Context::new()?;
+    ctx.register_callback(Default::default(), |device, event| {
+        eprintln!("invoked");
+        println!("{:?} - {:?}", device.device_descriptor(), event);
+    });
+    loop {
+        ctx.handle_events();
+    }
+    Ok(())
+}
+
